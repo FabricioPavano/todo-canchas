b1699f93d3b64339a3cd32b6fb357785af3ed7ae
Rodrigo Pavano <rodrigopavano@gmail.com>
2012-06-20 10:52:27 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todocanchas::Application.config.secret_token = '58149e19ce1e9e8ecdb0ed6dbadba913bc0edefabcbb6cb67942bfde64565c48235f80866205c80daec126408eafd85548c7462482b5069be293d7dd2728676f'
