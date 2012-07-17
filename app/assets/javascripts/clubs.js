ee3803ab853daf9ad79a315763e33d58c2e7a97b
Fabricio Pavano <faripavano@gmail.com>
2012-07-17 11:13:00 -0300

    js added to checkboxes to show quantity fields

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -4,19 +4,24 @@ $(document).ready(function(){
   //acuerdo a los checkboxes
   function mostrarCamposCantidadCanchas(){
     
-    if($('#checkbox-futbol').is(':checked') == $('#cantidad-futbol').hasClass('hidden'))
-      $('#cantidad-futbol').toggleClass('hidden')    
     
-    if($('#checkbox-tenis').is(':checked') == $('#cantidad-tenis').hasClass('hidden'))
-      $('#cantidad-tenis').toggleClass('hidden')    
 
-    if($('#checkbox-paddle').is(':checked') == $('#cantidad-paddle').hasClass('hidden'))
-      $('#cantidad-paddle').toggleClass('hidden')    
+
+    if($('#club_courts_types_futbol').is(':checked') == $('#club_futbol_quantity').parents('.control-group').hasClass('hidden'))
+      $('#club_futbol_quantity').parents('.control-group').toggleClass('hidden')    
+    
+    if($('#club_courts_types_tenis').is(':checked') == $('#club_tenis_quantity').parents('.control-group').hasClass('hidden'))
+      $('#club_tenis_quantity').parents('.control-group').toggleClass('hidden')    
+
+    if($('#club_courts_types_paddle').is(':checked') == $('#club_paddle_quantity').parents('.control-group').hasClass('hidden'))
+      $('#club_paddle_quantity').parents('.control-group').toggleClass('hidden')   
+
+
 
   }
 
   //asginamos el handler 
-  $('.checkbox-canchas').click(mostrarCamposCantidadCanchas)
+  $('div.check_boxes').click(mostrarCamposCantidadCanchas)
 
 
 
